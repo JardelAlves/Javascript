67aa89736265073264e0024c71bb7a3d944bc261
Jardel Alves <jardelfilho2001@gmail.com>
2019-06-04 13:02:02 -0300

    Adicionando arquivos 7 e 8 do Fundamentals

--- Fundamentals/7-Operators.js
+++ Fundamentals/7-Operators.js
@@ -1,5 +1,17 @@
 // Concatenação de strings e operador '+':
 let string = 'string ' + 'aleatória'
+let conversion = '1' - '2'
 
-console.log(string)
+console.log(string, conversion)
 
+let a = 1, b = 1;
+
+let c = ++a; 
+let d = b++; 
+
+console.log(c, d)
+
+a = 2;
+let x = 1 + (a *= 2);
+
+console.log(a, x)
---
+++ Fundamentals/8-Comparisons.js
@@ -0,0 +1,15 @@
+//Para comparação entre string, o Javascript compara cada caractere para definir qual string é a maior
+//Utiliza o Unicode para fazer a comparação
+
+console.log('Green' < 'Grow')
+
+//Comparação estrita, faz a comparação sem alterar o tipo  das variáveis
+console.log(1 === '1')
+
+console.log(5 > 4,
+            "apple" > "pineapple",
+            "2" > "12",
+            undefined == null,
+            undefined === null,
+            null == "\n0\n",
+            null === +"\n0\n")
